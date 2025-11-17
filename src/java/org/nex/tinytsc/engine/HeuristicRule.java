03ad7c2c5ee9a94c0480370c15e496ae18e0d91d
Jack Park <jackpark@gmail.com>
2025-11-17 14:44:59 -0800

    Starting new rule type

---
+++ src/java/org/nex/tinytsc/engine/HeuristicRule.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package org.nex.tinytsc.engine;
+
+import org.nex.tinytsc.api.IConstants;
+import org.nex.tinytsc.api.Identifiable;
+
+/**
+ * 
+ */
+public class HeuristicRule implements Identifiable {
+	private final int type = IConstants.PROCESS_RULE;
+
+	/**
+	 * 
+	 */
+	public HeuristicRule() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toXML() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getNodeType() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
