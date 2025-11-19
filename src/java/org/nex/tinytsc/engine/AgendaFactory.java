95d67782b3441d6e778394e8f04dbc489be375b6
Jack Park <jackpark@gmail.com>
2025-11-18 16:11:21 -0800

    coded

--- src/java/org/nex/tinytsc/engine/AgendaFactory.java
+++ src/java/org/nex/tinytsc/engine/AgendaFactory.java
@@ -2,17 +2,34 @@
  * 
  */
 package org.nex.tinytsc.engine;
-
+import java.util.*;
 /**
  * 
  */
 public class AgendaFactory {
-
+	/**
+	 * Key: agenda name
+	 * Value: Agenda for that name
+	 */
+	private Map<String, AgendaManager2> agendas;
 	/**
 	 * 
 	 */
 	public AgendaFactory() {
-		// TODO Auto-generated constructor stub
+		agendas = new HashMap<String, AgendaManager2>();
 	}
 
+	/**
+	 * 
+	 * @param name
+	 * @return 
+	 */
+	public AgendaManager2 getOrCreateAgenda(String name) {
+		AgendaManager2 result = agendas.get(name);
+		if (result == null) {
+			result = new AgendaManager2();
+			agendas.put(name, result);
+		}
+		return result;
+	}
 }
