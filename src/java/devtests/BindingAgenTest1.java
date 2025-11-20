521e8ac4f8e52e43a504dd50b5e41aca71413671
Jack Park <jackpark@gmail.com>
2025-11-20 14:50:34 -0800

    Starting binding tests for QP engine

---
+++ src/java/devtests/BindingAgenTest1.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package devtests;
+
+import org.nex.tinytsc.engine.SimpleBindingEngine;
+
+/**
+ * 
+ */
+public class BindingAgenTest1 extends BaseTestClass {
+
+	/**
+	 * 
+	 */
+	public BindingAgenTest1() {
+		SimpleBindingEngine agent = new SimpleBindingEngine(environment);
+		// Start by constructing a list of rule Sentences and episode sentences
+		// returns a boolean if they bind
+		
+		
+		
+		System.exit(0);
+	}
+
+}
